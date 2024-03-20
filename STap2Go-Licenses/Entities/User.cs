79f297c7f132453167f6d27c41007fc33dbf2eeb
smaringen <smarin@geneticai.com>
2024-03-20 13:19:18 +0100

    Creación entidad usuario

---
+++ STap2Go-Licenses/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿namespace STap2Go_Licenses.Entities
+{
+    public class User
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public bool? IsCompany { get; set; }
+        public string? ContactNIF { get; set; }
+        public string? CompanyName { get; set; }
+        public string? CompanyNIF { get; set; }
+        public string Address1 { get; set; } = string.Empty;
+        public string? Address2 { get; set; }
+        public string PostalCode { get; set; } = string.Empty;
+        public string Province { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+}
