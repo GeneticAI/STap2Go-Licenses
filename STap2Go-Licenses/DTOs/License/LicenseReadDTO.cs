f6b39ac5ffd9673bf50e4038220d24289cf7f2ea
smaringen <smarin@geneticai.com>
2024-03-22 14:57:19 +0100

    DTO para leer licencias

---
+++ STap2Go-Licenses/DTOs/License/LicenseReadDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace STap2Go_Licenses.DTOs.License
+{
+    public class LicenseReadDTO
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int ProductId { get; set; }
+        public string LicenseCode { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; }
+        public DateTime? AssignmentDate { get; set; }
+        public DateTime? UsageDate { get; set; }
+    }
+}
