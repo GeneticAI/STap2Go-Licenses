8fae2c96381a8dd012a3b7e2ccead30ac9dd7c04
smaringen <smarin@geneticai.com>
2024-03-25 15:19:42 +0100

    DTO para reservar licencia de test

---
+++ STap2Go-Licenses/DTOs/License/LicenseReserveDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace STap2Go_Licenses.DTOs.License
+{
+    public class LicenseReserveDTO
+    {
+        [Required]
+        public string TargetEmail { get; set; } = string.Empty;
+    }
+}
